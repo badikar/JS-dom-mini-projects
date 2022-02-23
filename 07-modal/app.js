e0cc81fbbcbab96c89f61c288088b896434f3ab2
Karol Badeja <karolbadeja@yahoo.com>
2022-02-23 16:12:34 +0000

    modal

---
+++ 07-modal/app.js
@@ -0,0 +1,10 @@
+const openBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+openBtn.addEventListener('click', () => {
+  modal.classList.add('open-modal');
+});
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open-modal');
+});
