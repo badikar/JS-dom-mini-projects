6f7fbb6b0b6f93de2fdb1d055d9312afcd4a0f08
Karol Badeja <karolbadeja@yahoo.com>
2022-05-05 16:04:03 +0100

    paginate

--- 28-ES6-pagination/displayButtons.js
+++ 28-ES6-pagination/displayButtons.js
@@ -1,3 +1,12 @@
-const displayButtons = () => {}
+const displayButtons = (container, pages, activeIndex) => {
+  let btns = pages.map((_, pageIndex) => {
+    return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : 'null'}"
+    data-index="${pageIndex}"
+    >${pageIndex + 1}</btn>`;
+  });
+  btns.push('<button class="next-btn">next</btn>');
+  btns.unshift('<button class="prev-btn">prev</btn>');
+  container.innerHTML = btns.join('');
+};
 
-export default displayButtons
+export default displayButtons;
--- 28-ES6-pagination/displayFollowers.js
+++ 28-ES6-pagination/displayFollowers.js
@@ -1,3 +1,19 @@
-const display = () => {}
+const container = document.querySelector('.container');
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      const { avatar_url, html_url, login } = person;
+
+      return `<article class="card">
+        <img src="${avatar_url}" alt="${login}/>
+        <h4>${login}</h4>
+        <a href="${html_url}" class="btn">view profile</a>
+      </article>`;
+    })
+    .join('');
+
+  container.innerHTML = newFollowers;
+};
+
+export default display;
